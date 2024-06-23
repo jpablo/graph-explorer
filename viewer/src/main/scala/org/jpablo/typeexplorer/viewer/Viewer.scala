b8945ab2c7adc997d6d62165f6861de742aa310a
Juan Pablo Romero <jpablo.romero@gmail.com>
2024-06-23 14:24:49 -0700

    refactoring

--- viewer/src/main/scala/org/jpablo/typeexplorer/viewer/Viewer.scala
+++ viewer/src/main/scala/org/jpablo/typeexplorer/viewer/Viewer.scala
@@ -3,24 +3,20 @@ package org.jpablo.typeexplorer.viewer
 import com.raquo.laminar.api.L.*
 import com.raquo.laminar.nodes.ReactiveHtmlElement
 import com.softwaremill.quicklens.*
+import org.jpablo.typeexplorer.viewer.backends.graphviz.Graphviz
 import org.jpablo.typeexplorer.viewer.components.state.*
 import org.jpablo.typeexplorer.viewer.components.{SvgDiagram, TopLevel}
 import org.jpablo.typeexplorer.viewer.examples.Example1
 import org.jpablo.typeexplorer.viewer.graph.ViewerGraph
 import org.scalajs.dom
-import org.scalajs.dom.SVGSVGElement
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import scala.scalajs.js
-import scala.scalajs.js.Promise
 
 object Viewer:
 
   def main(args: Array[String]): Unit =
     val graph = Signal.fromValue(Example1.diagram)
-    val viz = new GraphViz
-    val appElem = createApp(graph, buildSvgDiagram(viz))
+    val viz = new Graphviz
+    val appElem = createApp(graph, viz.renderDot)
     render(dom.document.querySelector("#app"), appElem)
 
   private def createApp(
@@ -35,11 +31,6 @@ object Viewer:
     val viewerState = ViewerState(appState.activeProject.pageV, appState.fullGraph, renderDot)
     TopLevel(appState, viewerState, graph, viewerState.svgDiagram)
 
-  private def buildSvgDiagram(viz: GraphViz)(s: String): Signal[SvgDiagram] =
-    Signal
-      .fromFuture(viz.render(s).map(SvgDiagram.apply))
-      .map(_.getOrElse(SvgDiagram.empty))
-
   private def setupErrorHandling()(using Owner): EventBus[String] =
     val errors = new EventBus[String]
     AirstreamError.registerUnhandledErrorCallback: ex =>
@@ -47,13 +38,3 @@ object Viewer:
     windowEvents(_.onError).foreach: e =>
       errors.emit(e.message)
     errors
-
-class GraphViz:
-  private val instance: Future[js.Dynamic] =
-    js.Dynamic.global.Viz
-      .instance()
-      .asInstanceOf[js.Promise[js.Dynamic]]
-      .toFuture
-
-  def render(g: String): Future[SVGSVGElement] =
-    instance.map(_.renderSVGElement(g).asInstanceOf[SVGSVGElement])
--- viewer/src/main/scala/org/jpablo/typeexplorer/viewer/backends/graphviz/Graphviz.scala
+++ viewer/src/main/scala/org/jpablo/typeexplorer/viewer/backends/graphviz/Graphviz.scala
@@ -1,8 +1,31 @@
 package org.jpablo.typeexplorer.viewer.backends.graphviz
 
+import com.raquo.laminar.api.L.*
+import org.jpablo.typeexplorer.viewer.components.SvgDiagram
 import org.jpablo.typeexplorer.viewer.graph.ViewerGraph
 import org.jpablo.typeexplorer.viewer.models
 import org.jpablo.typeexplorer.viewer.components.state.{DiagramOptions, ProjectSettings, SymbolOptions}
+import org.scalajs.dom.SVGSVGElement
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.scalajs.js
+
+class Graphviz:
+  private val instance: Future[js.Dynamic] =
+    js.Dynamic.global.Viz
+      .instance()
+      .asInstanceOf[js.Promise[js.Dynamic]]
+      .toFuture
+
+  private def renderSVGElement(g: String): Future[SVGSVGElement] =
+    instance.map(_.renderSVGElement(g).asInstanceOf[SVGSVGElement])
+
+  def renderDot(s: String): Signal[SvgDiagram] =
+    Signal
+      .fromFuture(renderSVGElement(s).map(SvgDiagram.apply))
+      .map(_.getOrElse(SvgDiagram.empty))
+
 
 object Graphviz:
 
@@ -31,5 +54,3 @@ object Graphviz:
        |
        |}
        |""".stripMargin
-
-end Graphviz
