5575d6f34344cd2e12d4f3379ab03dacc02ab716
Juan Pablo Romero <jpablo.romero@gmail.com>
2024-07-21 13:27:36 -0700

    loading of parser is working

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 import './style.css'
 import './style.scss'
+import './dotParser.js'
 import '@public/main.js'
--- viewer/src/main/scala/org/jpablo/typeexplorer/viewer/backends/graphviz/DotParser.scala
+++ viewer/src/main/scala/org/jpablo/typeexplorer/viewer/backends/graphviz/DotParser.scala
@@ -1,14 +1,11 @@
 package org.jpablo.typeexplorer.viewer.backends.graphviz
 
 import scala.scalajs.js
-import org.scalajs.dom
 import scala.scalajs.js.annotation.*
 
 @js.native
-@JSGlobal("DotParser") // JavaScriptException: ReferenceError: DotParser is not defined
+@JSImport("/dotParser.js", JSImport.Namespace)
 object DotParser extends js.Object {
-  dom.console.log("[DotParser] init")
-  def parse(dot: String): js.Object = js.native
-  def SyntaxError: js.Dynamic = js.native
-  def StartRules: js.Array[String] = js.native
+  def parse(dotString: String): js.Object = js.native
 }
+
