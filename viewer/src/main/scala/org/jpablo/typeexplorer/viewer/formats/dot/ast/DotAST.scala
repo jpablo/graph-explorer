608ec1d392a560e33a467dd54734f67973e364b6
Juan Pablo Romero <jpablo.romero@gmail.com>
2024-07-28 10:22:32 -0700

    style

--- viewer/src/main/scala/org/jpablo/typeexplorer/viewer/formats/dot/ast/DotAST.scala
+++ viewer/src/main/scala/org/jpablo/typeexplorer/viewer/formats/dot/ast/DotAST.scala
@@ -1,17 +1,15 @@
 package org.jpablo.typeexplorer.viewer.formats.dot.ast
 
+import org.jpablo.typeexplorer.viewer.formats.dot.ast.Location.Position
 import upickle.implicits.key
 import upickle.default.*
 
-case class DiGraph(
-    location: Location,
-    children: List[GraphElement],
-    id:       String
-) derives ReadWriter
+case class DiGraph(location: Location, children: List[GraphElement], id: String) derives ReadWriter
 
 case class Location(start: Position, end: Position) derives ReadWriter
 
-case class Position(offset: Int, line: Int, column: Int) derives ReadWriter
+object Location:
+  case class Position(offset: Int, line: Int, column: Int) derives ReadWriter
 
 @key("type")
 sealed trait GraphElement derives ReadWriter:
@@ -33,27 +31,20 @@ case class AttrStmt(
     derives ReadWriter
 
 @key("attr")
-case class Attr(
-    location: Location,
-    id:       String,
-    @key("eq")
-    attrEq: String
-) derives ReadWriter
+case class Attr(location: Location, id: String, @key("eq") attrEq: String) derives ReadWriter
 
 @key("edge_stmt")
 case class EdgeStmt(
     location: Location,
     @key("edge_list")
-    edgeList: List[DotNodeId],
+    edgeList: List[EdgeStmt.DotNodeId],
     @key("attr_list")
     attrList: List[Attr]
 ) extends GraphElement
     derives ReadWriter
 
-case class DotNodeId(
-    location: Location,
-    id:       String
-) derives ReadWriter
+object EdgeStmt:
+  case class DotNodeId(location: Location, id: String) derives ReadWriter
 
 @key("stmt_sep")
 case class StmtSep(location: Location) extends GraphElement derives ReadWriter
