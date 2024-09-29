f77ecfacb30c58ee89eb0e68bba2e67086a4d6f6
Juan Pablo Romero <jpablo.romero@gmail.com>
2024-09-28 17:32:32 -0700

    add a few failing examples

--- viewer/src/main/scala/org/jpablo/graphexplorer/viewer/backends/graphviz/DotExamples.scala
+++ viewer/src/main/scala/org/jpablo/graphexplorer/viewer/backends/graphviz/DotExamples.scala
@@ -7,5 +7,6 @@ object DotExamples:
   val examples = VectorMap(
     "Empty Graph"          -> "/examples/empty-graph.dot",
     "Finite State Machine" -> "/examples/finite-state-machine.dot",
-    "Clusters"             -> "/examples/clusters.dot"
+    "Clusters"             -> "/examples/clusters.dot",
+    "Styles"               -> "/examples/styles.dot"
   )
