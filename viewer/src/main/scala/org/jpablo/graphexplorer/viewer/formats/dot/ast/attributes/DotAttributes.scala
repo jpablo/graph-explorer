fa6a15630109cec24557ab66cc4d4630b4370ac3
Juan Pablo Romero <jpablo.romero@gmail.com>
2024-11-18 01:34:44 -0800

    Add Mrecord Shape back

--- viewer/src/main/scala/org/jpablo/graphexplorer/viewer/formats/dot/ast/attributes/DotAttributes.scala
+++ viewer/src/main/scala/org/jpablo/graphexplorer/viewer/formats/dot/ast/attributes/DotAttributes.scala
@@ -60,7 +60,7 @@ enum Shape:
 //    box3d, component
   case box, polygon, ellipse, oval, circle, point, egg, triangle, plaintext, plain, diamond, trapezium, parallelogram,
     house, pentagon, hexagon, septagon, octagon, doublecircle, doubleoctagon, tripleoctagon, invtriangle, invtrapezium,
-    invhouse, Mdiamond, Msquare, Mcircle, rect, rectangle, square, star, underline, cylinder, note, tab, folder, box3d,
+    invhouse, Mdiamond, Msquare, Mcircle, Mrecord, rect, rectangle, square, star, underline, cylinder, note, tab, folder, box3d,
     component, promoter, cds, terminator, utr, primersite, restrictionsite, fivepoverhang, threepoverhang, noverhang,
     assembly, signature, insulator, ribosite, rnastab, proteasesite, proteinstab, rarrow, larrow, lpromoter, rpromoter
 
