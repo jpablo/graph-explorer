92cdf5ca69a7d237f244d55fc63b5122bc660f36
Juan Pablo Romero <jpablo.romero@gmail.com>
2024-11-18 11:17:22 -0800

    simplification

--- viewer/src/main/scala/org/jpablo/graphexplorer/viewer/components/leftPanel/LeftPanel.scala
+++ viewer/src/main/scala/org/jpablo/graphexplorer/viewer/components/leftPanel/LeftPanel.scala
@@ -15,86 +15,116 @@ def LeftPanel(state: ViewerState) =
   val filterEdgesByNodeId = Var("")
   val onlyActiveNodes = Var(false)
   val onlyActiveEdges = Var(false)
+
   def isVisible(i: Int) = visibleTab.signal.map(_ == i)
 
   div(
     idAttr := "nodes-panel",
-    // --- First row (misc) ---
-    div(
-      cls := "flex gap-2 justify-between ml-10",
-      Select(
-        placeholderText = "Select example",
-        options         = examples.keys.map(name => name -> name),
-        onChange.mapToValue.map(examples).flatMap(FetchStream.get(_)) --> { source =>
-          state.showAllNodes()
-          state.sourceText.set(source)
-        }
-      ),
-      a(
-        cls    := "link",
-        href   := "https://www.graphviz.org/documentation/",
-        target := "_blank",
-        title  := "Visit the Graphviz documentation for more information",
-        "Graphviz"
-      )
-    ),
+    FirstRow(state),
     // --- Tab Headers ---
     div(
       idAttr := "nodes-panel-tab-buttons",
-      // Header 0: Attributes
-      Button("Style", cls("btn-active") <-- isVisible(0), onClick --> visibleTab.set(0)).tiny,
+      TabHeaderStyle(visibleTab, isVisible, 0),
+      TabHeaderSource(visibleTab, isVisible, 1),
+      TabHeaderNodes(visibleTab, state, isVisible, 2),
+      TabHeaderEdges(visibleTab, state, isVisible, 3)
+    ),
+    // --- Tab Body ---
+    TabStyle(state, isVisible, 0),
+    TabSource(state, isVisible, 1),
+    TabNodes(state, filterNodesByNodeId, onlyActiveNodes, isVisible, 2),
+    TabEdges(state, filterEdgesByNodeId, onlyActiveEdges, isVisible, 3)
+  )
 
-      // Header 1: Source
-      Button("Source", cls("btn-active") <-- isVisible(1), onClick --> visibleTab.set(1)).tiny,
-      // Header 2: Nodes
-      Button(
-        child <-- state.fullGraph.map(_.summary.nodes).map(n => s"Nodes ($n)"),
-        cls("btn-active") <-- isVisible(2),
-        onClick --> visibleTab.set(2)
-      ).tiny,
-      // Header 3: Edges
-      Button(
-        child <-- state.fullGraph.map(_.summary.arrows).map(n => s"Edges ($n)"),
-        cls("btn-active") <-- isVisible(3),
-        onClick --> visibleTab.set(3)
-      ).tiny
+def FirstRow(state: ViewerState) =
+  div(
+    cls := "flex gap-2 justify-between ml-10",
+    Select(
+      placeholderText = "Select example",
+      options = examples.keys.map(name => name -> name),
+      onChange.mapToValue.map(examples).flatMap(FetchStream.get(_)) --> { source =>
+        state.showAllNodes()
+        state.sourceText.set(source)
+      }
     ),
-    // ------ TAB 0: Style ------
-    div(
-      cls("hidden") <-- !isVisible(0),
-      DiagramAttributesView(state)
-    ),
-    // ------ TAB 1: Source ------
-    // --- DOT sources ---
-    CodeMirror(
-      state.sourceText,
-      idAttr := "nodes-source",
-      cls("hidden") <-- !isVisible(1),
-      placeholder := "DOT source"
-    ),
+    a(
+      cls := "link",
+      href := "https://www.graphviz.org/documentation/",
+      target := "_blank",
+      title := "Visit the Graphviz documentation for more information",
+      "Graphviz"
+    )
+  )
 
-    // ------ TAB 2: Nodes ------
-    // --- controls ---
+def TabHeaderStyle(visibleTab: Var[Int], isVisible: Int => Signal[Boolean], idx: Int) =
+  Button("Style", cls("btn-active") <-- isVisible(idx), onClick --> visibleTab.set(idx)).tiny
+
+def TabHeaderSource(visibleTab: Var[Int], isVisible: Int => Signal[Boolean], idx: Int) =
+  Button("Source", cls("btn-active") <-- isVisible(idx), onClick --> visibleTab.set(idx)).tiny
+
+def TabHeaderNodes(visibleTab: Var[Int], state: ViewerState, isVisible: Int => Signal[Boolean], idx: Int) =
+  Button(
+    child <-- state.fullGraph.map(_.summary.nodes).map(n => s"Nodes ($n)"),
+    cls("btn-active") <-- isVisible(idx),
+    onClick --> visibleTab.set(idx)
+  ).tiny
+
+def TabHeaderEdges(visibleTab: Var[Int], state: ViewerState, isVisible: Int => Signal[Boolean], idx: Int) =
+  Button(
+    child <-- state.fullGraph.map(_.summary.arrows).map(n => s"Edges ($n)"),
+    cls("btn-active") <-- isVisible(idx),
+    onClick --> visibleTab.set(idx)
+  ).tiny
+
+def TabStyle(state: ViewerState, isVisible: Int => Signal[Boolean], idx: Int) =
+  div(
+    cls("hidden") <-- !isVisible(idx),
+    DiagramAttributesView(state)
+  )
+
+def TabSource(state: ViewerState, isVisible: Int => Signal[Boolean], idx: Int) =
+  CodeMirror(
+    state.sourceText,
+    idAttr := "nodes-source",
+    cls("hidden") <-- !isVisible(idx),
+    placeholder := "DOT source"
+  )
+
+def TabNodes(
+    state:               ViewerState,
+    filterNodesByNodeId: Var[String],
+    onlyActiveNodes:     Var[Boolean],
+    isVisible:           Int => Signal[Boolean],
+    idx:                 Int
+) =
+  div(
     form(
       idAttr := "nodes-panel-controls",
-      cls("hidden") <-- !isVisible(2),
+      cls("hidden") <-- !isVisible(idx),
       Join(LabeledCheckbox(id = s"filter-by-active", labelStr = "only visible", isChecked = onlyActiveNodes)),
       Search(
         placeholder := "filter",
         controlled(value <-- filterNodesByNodeId, onInput.mapToValue --> filterNodesByNodeId)
       ).smallInput
     ),
-    // Scrollable content
     div(
       idAttr := "nodes-panel-contents",
-      cls("hidden") <-- !isVisible(2),
-      // List of nodes
+      cls("hidden") <-- !isVisible(idx),
       NodesList(state, onlyActiveNodes.signal, filterNodesByNodeId.signal)
-    ),
-    // ------ TAB 3: Edges ------
+    )
+  )
+
+def TabEdges(
+    state:               ViewerState,
+    filterEdgesByNodeId: Var[String],
+    onlyActiveEdges:     Var[Boolean],
+    isVisible:           Int => Signal[Boolean],
+    idx:                 Int
+) =
+  div(
     form(
       idAttr := "edges-panel-controls",
-      cls("hidden") <-- !isVisible(3),
+      cls("hidden") <-- !isVisible(idx),
       Join(LabeledCheckbox(id = s"filter-by-active", labelStr = "only visible", isChecked = onlyActiveEdges)),
       Search(
         placeholder := "filter",
@@ -103,7 +133,7 @@ def LeftPanel(state: ViewerState) =
     ),
     div(
       idAttr := "edges-panel-contents",
-      cls("hidden") <-- !isVisible(3),
+      cls("hidden") <-- !isVisible(idx),
       EdgesList(state, onlyActiveEdges, filterEdgesByNodeId.signal)
     )
   )
