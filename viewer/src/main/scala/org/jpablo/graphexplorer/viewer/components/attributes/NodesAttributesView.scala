09bc679fb467a815283b7439f88d5d4faed68ee6
Juan Pablo Romero <jpablo.romero@gmail.com>
2025-01-20 23:58:23 -0800

    simplification

--- viewer/src/main/scala/org/jpablo/graphexplorer/viewer/components/attributes/NodesAttributesView.scala
+++ viewer/src/main/scala/org/jpablo/graphexplorer/viewer/components/attributes/NodesAttributesView.scala
@@ -8,7 +8,6 @@ import org.jpablo.graphexplorer.viewer.widgets.InputType
 import org.jpablo.graphexplorer.viewer.widgets.InputType.{checkbox, color, number}
 import org.jpablo.graphexplorer.viewer.extensions.extraAttributes.FillStyle
 import org.jpablo.graphexplorer.viewer.components.attributes.AttributeType.buildRow
-import org.jpablo.graphexplorer.viewer.extensions.in
 
 def NodesAttributesView(attrsVar: Var[Map[String, AttrValue]], selection: Boolean) =
 
@@ -47,48 +46,50 @@ def NodesAttributesView(attrsVar: Var[Map[String, AttrValue]], selection: Boolea
   )
 
 private def fillStyleVar(attrsVar: Var[Map[String, AttrValue]]): Var[Option[AttrValue]] =
-  styleVar(attrsVar, _.contains(FillStyle.ColorFill.toString), defaultValue = Some("solid"))
-
-private def borderStyleVar(attrsVar: Var[Map[String, AttrValue]]): Var[Option[AttrValue]] =
-  styleVar(attrsVar, _.nonEmpty, keepStyle = true, defaultValue = Some("solid"))
-
-private def styleVar(
-    attrsVar: Var[Map[String, AttrValue]], 
-    shouldIncludeStyle: String => Boolean,
-    keepStyle: Boolean = false,
-    defaultValue: Option[String] = None
-): Var[Option[AttrValue]] =
   val filled = "filled"
-  
+
+  def getCurrentValue(attrs: Map[String, AttrValue]): Option[AttrValue] =
+    attrs.get(Style.attrId)
+      .map: style =>
+        AttrValue(
+          (if style.toString.contains(filled) then FillStyle.ColorFill else FillStyle.NoFill).toString
+        )
+
+  def updateStyles(attrs: Map[String, AttrValue], value: Option[AttrValue]): Map[String, AttrValue] =
+    value.fold(attrs) { v =>
+      val currentStyles = attrs.get(Style.attrId).map(_.toString).map(parseStyles).getOrElse(Set.empty)
+      val newStyles = 
+        if v.toString.contains(FillStyle.ColorFill.toString) then
+          currentStyles + filled
+        else 
+          currentStyles - filled
+      if newStyles.isEmpty then attrs - Style.attrId
+      else attrs + (Style.attrId -> AttrValue(newStyles.mkString(",")))
+    }
+
+  attrsVar.zoomLazy(getCurrentValue)(updateStyles)
+
+// handle "style" attribute by ignoring any "filled" value present
+private def borderStyleVar(attrsVar: Var[Map[String, AttrValue]]): Var[Option[AttrValue]] =
+  val filled = "filled"
+
   def getCurrentValue(attrs: Map[String, AttrValue]): Option[AttrValue] =
     attrs.get(Style.attrId)
       .map(_.toString)
       .map: style =>
         val styles = parseStyles(style)
-        if !keepStyle then
-          AttrValue(if filled in styles then FillStyle.ColorFill.toString else FillStyle.NoFill.toString)
-        else if filled in styles then
-          val value = (styles - filled).mkString(",")
-          AttrValue(if value.isEmpty then defaultValue.getOrElse("") else value)
-        else
-          AttrValue(defaultValue.getOrElse(""))
+        val value = (styles - filled).mkString(",")
+        AttrValue(if value.isEmpty then Style.default.toString else value)
 
   def updateStyles(attrs: Map[String, AttrValue], value: Option[AttrValue]): Map[String, AttrValue] =
     value.fold(attrs) { v =>
       val currentStyles = attrs.get(Style.attrId).map(_.toString).map(parseStyles).getOrElse(Set.empty)
-      pprint.log((currentStyles, v, keepStyle, defaultValue))
       val newStyles = 
-        if keepStyle then
-          if v.toString.isEmpty then
-            currentStyles.filter(_ == filled)
-          else
-            parseStyles(v.toString) ++ currentStyles.filter(_ == filled)
+        if v.toString.isEmpty then
+          currentStyles.filter(_ == filled)
         else
-          if shouldIncludeStyle(v.toString) then
-            currentStyles + filled
-          else 
-            currentStyles - filled
-      if newStyles.isEmpty && defaultValue.isEmpty then attrs - Style.attrId
+          parseStyles(v.toString) ++ currentStyles.filter(_ == filled)
+      if newStyles.isEmpty then attrs - Style.attrId
       else attrs + (Style.attrId -> AttrValue(newStyles.mkString(",")))
     }
 
