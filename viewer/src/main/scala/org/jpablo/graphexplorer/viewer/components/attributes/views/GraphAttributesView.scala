1d4c66a393745a6c1cd71af0fd6be223747adaa4
Juan Pablo Romero <jpablo.romero@gmail.com>
2025-03-05 15:56:05 -0800

    refactor: Simplify graph attribute row definitions by removing unused sub attributes and streamlining input rows

--- viewer/src/main/scala/org/jpablo/graphexplorer/viewer/components/attributes/views/GraphAttributesView.scala
+++ viewer/src/main/scala/org/jpablo/graphexplorer/viewer/components/attributes/views/GraphAttributesView.scala
@@ -3,17 +3,15 @@ package org.jpablo.graphexplorer.viewer.components.attributes.views
 import com.raquo.airstream.state.Var
 import com.raquo.laminar.api.L.*
 import org.jpablo.graphexplorer.viewer.components.attributes.previews.BorderStylePreview
-import org.jpablo.graphexplorer.viewer.components.attributes.rows.RowBuilder
 import org.jpablo.graphexplorer.viewer.components.attributes.rows.AttributeRow.RowOption
-import org.jpablo.graphexplorer.viewer.components.attributes.style.{CommonSubAttributes, StyleSubAttributes}
+import org.jpablo.graphexplorer.viewer.components.attributes.rows.RowBuilder
 import org.jpablo.graphexplorer.viewer.formats.dot.ast.AttrValue
 import org.jpablo.graphexplorer.viewer.formats.dot.ast.attributes.*
 import org.jpablo.graphexplorer.viewer.models.AttrStatus.Single
 import org.jpablo.graphexplorer.viewer.models.{Attributes, AttributesUpdates}
 import org.jpablo.graphexplorer.viewer.state.ViewerState
 import org.jpablo.graphexplorer.viewer.widgets.InputType
-import org.jpablo.graphexplorer.viewer.widgets.InputType.{color, number, range}
-
+import org.jpablo.graphexplorer.viewer.widgets.InputType.{checkbox, color, number, range}
 
 def GraphAttributesView(
     state:     ViewerState,
@@ -37,31 +35,13 @@ def GraphAttributesView(
     else
       builder.simpleRow(Label, InputType.multiText, onReset = Some(""), placeholder = Some("Enter group label"))
 
-  val defaultSubAttrs: Signal[StyleSubAttributes] =
-    defaults
-      .map(_.map(attrs => StyleSubAttributes.from(attrs).getOrElse(StyleSubAttributes.missing)))
-      .getOrElse(Signal.fromValue(StyleSubAttributes.missing))
-
-
-  val commonSubAttrs = CommonSubAttributes(attrsVar, defaultSubAttrs)
-  import commonSubAttrs.*
-
   val borderStyleRow =
     builder
-      .inputRow(BorderStyle -> InputType.selectWithPreview, borderStyle.getVar, borderStyle.getDefault)
+      .simpleRow(BorderStyle, InputType.selectWithPreview)
       .copy(
         options =
           BorderStyle.valuesWithLabel.toSeq.map: (label, style) =>
             RowOption(label, Single(AttrValue(style.toString)), BorderStylePreview(style))
-      )
-
-  val shapeModeStyleRow =
-    builder
-      .inputRow(CornerStyle -> InputType.select, cornerStyle.getVar, cornerStyle.getDefault)
-      .copy(
-        options =
-          CornerStyle.valuesWithLabel.filterNot(_._2 == CornerStyle.diagonals).toSeq.map: (label, style) =>
-            RowOption(label, Single(AttrValue(style.toString)), None)
       )
 
   AttributesView(
@@ -77,17 +57,17 @@ def GraphAttributesView(
       FontColor -> color,
       FontSize  -> number(start = Some(1), end = Some(100), step = Some(1)),
       "Style",
-      builder.inputRow(FillStyle -> InputType.checkbox, fillStyle.getVar, fillStyle.getDefault),
+      FillStyle -> checkbox,
       FillColor -> color,
       borderStyleRow,
-      PenWidth -> range(start = Some(0.0), end = Some(10.0), step = Some(0.1)),
-      PenColor -> color,
-      builder.inputRow(BoldStyle -> InputType.checkbox, boldStyle.getVar, boldStyle.getDefault),
-      shapeModeStyleRow
+      PenWidth  -> range(start = Some(0.0), end = Some(10.0), step = Some(0.1)),
+      PenColor  -> color,
+      BoldStyle -> checkbox,
+      CornerStyle
     ),
     if selection then
       builder.buildRows(
-        builder.inputRow(InvisibleStyle -> InputType.checkbox, invisibleStyle.getVar, invisibleStyle.getDefault),
+        InvisibleStyle -> checkbox,
         "Other",
         URL
       )
