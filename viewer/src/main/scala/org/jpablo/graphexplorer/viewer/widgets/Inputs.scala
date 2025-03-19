7027b16046a5d95d0688da6db8a8ee8b86252e26
Juan Pablo Romero <jpablo.romero@gmail.com>
2025-03-19 12:32:01 -0700

    style: Update TextAreaWithValue styling for improved appearance and usability

--- viewer/src/main/scala/org/jpablo/graphexplorer/viewer/widgets/Inputs.scala
+++ viewer/src/main/scala/org/jpablo/graphexplorer/viewer/widgets/Inputs.scala
@@ -264,7 +264,7 @@ def TextAreaWithValue(
     )
 
   textArea(
-    cls         := "textarea textarea-xs p-2 ",
+    cls         := "textarea textarea-xs p-1 min-h-[2rem] h-auto",
     placeholder := row.placeholder,
     value <-- rawText.signal,
     onInput.mapToValue --> rawText.set,
