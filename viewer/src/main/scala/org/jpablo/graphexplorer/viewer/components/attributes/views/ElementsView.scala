673ce52cef04c05f54d87a8f432cd060185b25ca
Juan Pablo Romero <jpablo.romero@gmail.com>
2025-03-19 10:52:42 -0700

    feat: Add ElementsView component for displaying Nodes and Arrows with tabbed interface

---
+++ viewer/src/main/scala/org/jpablo/graphexplorer/viewer/components/attributes/views/ElementsView.scala
@@ -0,0 +1,39 @@
+package org.jpablo.graphexplorer.viewer.components.attributes.views
+
+import com.raquo.laminar.api.L.*
+import org.jpablo.graphexplorer.viewer.components.rightPanel.{ArrowsList, NodesList}
+import org.jpablo.graphexplorer.viewer.state.ViewerState
+
+def ElementsView(state: ViewerState) =
+  val tabIndex = Var(0)
+  def tabVisible(i: Int) = tabIndex.signal.map(_ == i)
+  val summary = state.fullGraph.map(_.summary)
+
+  val tabsData =
+    List(
+      "Nodes"  -> NodesList(state),
+      "Arrows" -> ArrowsList(state)
+    )
+
+  div(
+    idAttr := "elements-view",
+    div(
+      div(
+        role := "tablist",
+        cls  := "tabs tabs-lift tabs-xs",
+        for (tabName, i) <- tabsData.map(_._1).zipWithIndex
+        yield a(
+          role := "tab",
+          cls  := "tab flex-1",
+          cls("tab-active") <-- tabVisible(i),
+          onClick.mapTo(i) --> tabIndex,
+          child <-- summary.map(s => s"$tabName (${if i == 0 then s.nodes else s.arrows})")
+        )
+      ),
+      div(
+        idAttr := "elements-view-content",
+        for (view, i) <- tabsData.map(_._2).zipWithIndex
+        yield view.amend(cls("hidden") <-- tabVisible(i).not)
+      )
+    )
+  )
