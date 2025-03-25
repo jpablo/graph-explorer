74943d467ce928bd59c8ed0cce4fd4d24b6ba242
Juan Pablo Romero <jpablo.romero@gmail.com>
2025-03-25 11:50:46 -0700

    refactor: Simplify GraphAttributesView by removing unused imports and enhancing row visibility logic for improved clarity and functionality

--- viewer/src/main/scala/org/jpablo/graphexplorer/viewer/components/attributes/views/GraphAttributesView.scala
+++ viewer/src/main/scala/org/jpablo/graphexplorer/viewer/components/attributes/views/GraphAttributesView.scala
@@ -2,32 +2,14 @@ package org.jpablo.graphexplorer.viewer.components.attributes.views
 
 import com.raquo.airstream.state.Var
 import com.raquo.laminar.api.L.*
-import org.jpablo.graphexplorer.viewer.components.attributes.previews.BorderStylePreview
+import io.laminext.syntax.core.*
 import org.jpablo.graphexplorer.viewer.components.attributes.rows.AttributeRow.RowOption
 import org.jpablo.graphexplorer.viewer.components.attributes.rows.RowBuilder
-import org.jpablo.graphexplorer.viewer.formats.dot.ColorType
-import org.jpablo.graphexplorer.viewer.formats.dot.ast.AttrValue
-import org.jpablo.graphexplorer.viewer.formats.dot.attributes.{
-  BoldStyle,
-  BorderStyle,
-  ClusterLabelLoc,
-  CornerStyle,
-  FillColor,
-  FillStyle,
-  FontColor,
-  FontSize,
-  InvisibleStyle,
-  Label,
-  LabelJust,
-  PenColor,
-  PenWidth,
-  URL
-}
-import org.jpablo.graphexplorer.viewer.models.AttrStatus.Single
+import org.jpablo.graphexplorer.viewer.formats.dot.attributes.{Label, *}
 import org.jpablo.graphexplorer.viewer.models.{Attributes, AttributesUpdates}
 import org.jpablo.graphexplorer.viewer.state.ViewerState
 import org.jpablo.graphexplorer.viewer.widgets.InputType
-import org.jpablo.graphexplorer.viewer.widgets.InputType.{checkbox, number, range}
+import org.jpablo.graphexplorer.viewer.widgets.InputType.range
 
 def GraphAttributesView(
     state:        ViewerState,
@@ -35,72 +17,36 @@ def GraphAttributesView(
     defaults:     Option[Signal[Attributes]] = None,
     defaultsView: Boolean
 ) =
-  val builder                 = RowBuilder(attrsVar, state.layout, defaults)
-  val isSingleClusterSelected = state.selection.signal.map(_.size == 1)
+  val multiSelection = state.selection.signal.map(_.size != 1)
+  val builder        = RowBuilder(attrsVar, state.layout, defaults)
+  import builder.{row, rows}
 
-  given owner: Owner = state.owner
+  val defaultsViewS = Signal.fromValue(defaultsView)
 
   val labelRow =
-    if defaultsView then
-      isSingleClusterSelected.map(single =>
-        if single then
-          builder.row(Label, InputType.multiText, onReset = Some(""), placeholder = Some("Enter group label"))
-        else
-          ""
-      ).observe(using state.owner).now()
-    else
-      builder.row(Label, InputType.multiText, onReset = Some(""), placeholder = Some("Enter group label"))
-
-  val borderStyleRow =
-    builder
-      .row(BorderStyle, InputType.selectWithPreview)
-      .copy(
-        options =
-          BorderStyle.valuesWithLabel.toSeq.map: (label, style) =>
-            RowOption(label, Single(AttrValue(style.toString)), BorderStylePreview(style))
-      )
-
-  val fillColorRowOpts =
-    ColorType.x11BasicColors.toSeq
-      .sortBy(_._2)(Ordering.String.reverse)
-      .map: (name, hex) =>
-        RowOption(
-          name,
-          Single(AttrValue(hex)),
-          Some(() => div(cls := s"w-8 h-4 rounded border-1 border-solid", styleAttr := s"background-color: $hex"))
-        )
-
-  val fillStyleRow = builder.row(FillStyle, checkbox)
-  val fillColorRow = builder.row(FillColor, InputType.selectWithPreviewGrid)
-    .copy(
-      options = fillColorRowOpts,
-      hidden =
-        Signal.combine(
-          builder.invalidLayout(FillColor),
-          fillStyleRow.combineDefaultBoolean.not
-        ).map(_ || _)
-    )
+    row(Label, InputType.multiText, onReset = Some("")).copy(hidden = multiSelection || defaultsViewS)
 
   AttributesView(
     id = "graph-attributes",
-    rows = builder.rows(
-      if defaultsView then "Labels" else "",
-      if defaultsView then labelRow else "",
-      if defaultsView then ClusterLabelLoc else "",
-      if defaultsView then LabelJust else "",
-      "Fonts",
-      builder.row(FontColor, InputType.selectWithPreviewGrid).copy(options = colorOptions),
-      FontSize -> number(start = Some(1), end = Some(100), step = Some(1)),
-      "Style",
-      fillStyleRow,
-      fillColorRow,
-      borderStyleRow,
+    rows = rows(
+      row(PenColor, InputType.menuWithExtra(4)).copy(options = colorOptions),
+      row(FillColor, InputType.menuWithExtra(4))
+        .copy(
+          options = colorOptions,
+          hidden = builder.invalidLayout(FillColor)
+        ),
+      row(BorderStyle, InputType.menuWithExtra(4)).copy(options = borderStyleOptions),
       PenWidth -> range(start = Some(0.0), end = Some(10.0), step = Some(0.1)),
-      builder.row(PenColor, InputType.selectWithPreviewGrid).copy(options = colorOptions),
-      BoldStyle -> checkbox,
-      CornerStyle,
-      if defaultsView then InvisibleStyle -> checkbox else "",
-      if defaultsView then "Other" else "",
-      if defaultsView then URL else ""
+      row(CornerStyle, InputType.menuWithExtra(4)).copy(options = graphCornerStyleOptions),
+      // --
+      labelRow,
+      row(ClusterLabelLoc, InputType.menuWithExtra(4)).copy(options = clusterVerticalAlignmentOptions),
+      row(LabelJust, InputType.menuWithExtra(4)).copy(options = horizontalAlignmentOptions),
+      row(FontColor, InputType.menuWithExtra(4)).copy(options = colorOptions),
+      row(FontName, InputType.select),
+      row(FontSize, range(start = Some(1), end = Some(100), step = Some(1))),
+
+      // --
+      if defaultsView then "" else URL
     )
   )
