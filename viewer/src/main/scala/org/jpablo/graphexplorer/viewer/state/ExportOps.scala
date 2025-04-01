f07efa51364dd7f5426707aeb82604c634f6d00d
Juan Pablo Romero <jpablo.romero@gmail.com>
2025-04-01 12:38:37 -0700

    fix: Replace for-comprehensions with direct value assignments in ExportOps methods. This prevents zombie callbacks

--- viewer/src/main/scala/org/jpablo/graphexplorer/viewer/state/ExportOps.scala
+++ viewer/src/main/scala/org/jpablo/graphexplorer/viewer/state/ExportOps.scala
@@ -7,35 +7,35 @@ trait ExportOps:
   this: ViewerState =>
 
   def copyAsFullDiagramSVG(): Unit =
-    for html <- finalSVG.map(_.ref.outerHTML) do
-      writeText(html)
-  
+    val html = finalSVG.map(_.ref.outerHTML).observe().now()
+    writeText(html)
+
   def copySelectionAsSVG(): Unit =
-    for svgElem <- finalSVG do
-      writeText(SvgElementOps(svgElem.ref).toSVGTextWithIds(selection.now()))
-  
+    val svgElem = finalSVG.observe().now()
+    writeText(SvgElementOps(svgElem.ref).toSVGTextWithIds(selection.now()))
+
   def copyAsDOT(): Unit =
-    for dot <- visibleDOT do
-      writeText(dot.value)
-  
+    val dot = visibleDOT.observe().now()
+    writeText(dot.value)
+
   def copyAsJSON(): Unit =
-    for ast <- sourceFlow.visibleAST do
-      writeText(writeJs(ast).toString)
-  
+    val ast = sourceFlow.visibleAST.observe().now()
+    writeText(writeJs(ast).toString)
+
   def printVisibleGraphToConsole(): Unit =
-    for graph <- visibleGraph do
-      // Don't remove this line!! it IS the actual functionality
-      pprint.log(graph, showFieldNames = true)
-      dom.console.log("Visible graph printed to the console")
-  
+    val graph = visibleGraph.observe().now()
+    // Don't remove this line!! it IS the actual functionality
+    pprint.log(graph, showFieldNames = true)
+    dom.console.log("Visible graph printed to the console")
+
   def printVisibleDOTtoConsole(): Unit =
-    for dotText <- visibleDOT do
-      // Don't remove this line!! it IS the actual functionality
-      dom.console.log(dotText.value)
-      dom.console.log("Visible DOT printed to the console")
-  
+    val dotText = visibleDOT.observe().now()
+    // Don't remove this line!! it IS the actual functionality
+    dom.console.log(dotText.value)
+    dom.console.log("Visible DOT printed to the console")
+
   def printVisibleJSONtoConsole(): Unit =
-    for ast <- sourceFlow.visibleAST do
-      // Don't remove this line!! it IS the actual functionality
-      dom.console.log(write(ast, indent = 2))
-      dom.console.log("Visible JSON DOT AST printed to the console")
+    val ast = sourceFlow.visibleAST.observe().now()
+    // Don't remove this line!! it IS the actual functionality
+    dom.console.log(write(ast, indent = 2))
+    dom.console.log("Visible JSON DOT AST printed to the console")
