41cd587b6848861a9b085d13761164b2bdfad20d
Juan Pablo Romero <jpablo.romero@gmail.com>
2025-04-20 23:39:42 -0700

    Refactor SVG canvas mouse action handling and selection logic

--- viewer/src/main/scala/org/jpablo/graphexplorer/viewer/components/svgCanvas/SvgCanvas.scala
+++ viewer/src/main/scala/org/jpablo/graphexplorer/viewer/components/svgCanvas/SvgCanvas.scala
@@ -36,89 +36,90 @@ def SvgCanvas(
   val magicY = -0.4
   val bbox   = BBox(viewBox.x - tr.x + magicX, viewBox.y - tr.y + magicY, viewBox.width, viewBox.height)
 
-  val reactiveFirstGroup = foreignSvgElement(svg.g, firstGroup).amend(svg.transform <-- transform)
+  emptySvg(
+    viewBox = bbox,
+    foreignSvgElement(svg.g, firstGroup)
+  ).amendThis { topLevelSvg =>
+    val selectionGroups =
+      selection.signal
+        .scanLeft(x => (ElementIds(), x)):
+          case ((_, curr), next) => (curr, next)
+        .map: (curr, next) =>
+          val toUnselect = curr.filter(id => !next.contains(id))
+          val toSelect   = next.filter(id => !curr.contains(id))
+          (SelectableElement.query(topLevelSvg.ref, toUnselect), SelectableElement.query(topLevelSvg.ref, toSelect))
 
-  emptySvg(viewBox = bbox, reactiveFirstGroup)
-    .amendThis { topLevelSvg =>
-      val selectionGroups =
-        selection.signal
-          .scanLeft(x => (ElementIds(), x)):
-            case ((_, curr), next) => (curr, next)
-          .map: (curr, next) =>
-            val toUnselect = curr.filter(id => !next.contains(id))
-            val toSelect   = next.filter(id => !curr.contains(id))
-            (SelectableElement.query(topLevelSvg.ref, toUnselect), SelectableElement.query(topLevelSvg.ref, toSelect))
+    val singleSelection =
+      selection.signal.map: selected =>
+        if selected.size == 1 then SelectableElement.query(topLevelSvg.ref, selected).headOption else None
 
-      val singleSelection =
-        selection.signal.map: selected =>
-          if selected.size == 1 then SelectableElement.query(topLevelSvg.ref, selected).headOption else None
+    val allSelectable =
+      SelectableElement.findAll(topLevelSvg.ref)
 
-      val allSelectable =
-        SelectableElement.findAll(topLevelSvg.ref)
+    val firstGroup: dom.svg.G =
+      topLevelSvg.ref.querySelectorT("g").getOrElse(throw Exception("No <g> element found in the SVG"))
 
-      val firstGroup: dom.svg.G =
-        topLevelSvg.ref.querySelectorT("g").getOrElse(throw Exception("No <g> element found in the SVG"))
-
-      Seq(
-        // mouseAction.signal.map(_.name).distinct --> { c => pprint.log(c) },
-        // --------------------------------------------------------
-        // Mouse events
-        // --------------------------------------------------------
-        // 1. Drawing a selecting rectangle starts here. Other actions start in their respective elements.
-        onMouseDown.filter(leftButton).map(clientCoords) --> { (pos, shift) =>
-          mouseAction.start(ExtendSelectionAction(MouseActionRect(pos, pos, shift)))
-        },
-        // 2. Any ongoing action is updated here (i.e., mouse position)
-        onMouseMove.filter(leftButtonMoved).map(clientCoords) --> mouseAction.updateEndpoint.tupled,
-        // 3. Any ongoing action ends here
-        onMouseUp.filter(leftButton)(_.withCurrentValueOf(mouseAction.signal)) --> { (ev, previousAction) =>
-          mouseAction.inactive()
-          previousAction match
-            case a: AddNewArrowAction       => viewerOps.handleAddNewArrowMouseUp(ev, a)
-            case a: MoveArrowEndpointAction => viewerOps.handleMoveArrowStartMouseUp(ev, a)
-            case a: ExtendSelectionAction   =>
-            case Inactive                   =>
-        },
-        // --------------------------------------------------------
-        // derived events
-        // --------------------------------------------------------
-        // selection rectangle
-        child.maybe <--
-          mouseAction.signal.map:
-            case a: ExtendSelectionAction => Some(viewerOps.DrawSelectionRect(topLevelSvg.ref, a))
-            case _                        => None,
-        // controls to initiate mouse actions
-        singleSelection.combineWith(mouseAction.signal) --> viewerOps.handleNewArrowControls(firstGroup).tupled,
-        singleSelection.combineWith(mouseAction.signal) --> viewerOps.handleArrowEndpointControl(firstGroup).tupled,
-        // dynamic arrow that follows the pointer
-        mouseAction.signal --> { action =>
-          // TODO: update the coordinates instead of recreating the arrow
-          firstGroup.querySelectorAll("g#dragging-arrow-group").foreach(_.remove())
-          action match
-            case a: AddNewArrowAction if !a.rect.isEmpty       => viewerOps.addArrowFromSourceToPointer(firstGroup, a)
-            case a: MoveArrowEndpointAction if !a.rect.isEmpty => viewerOps.addArrowBetweenPointerAndEndpoint(firstGroup, a)
-            case _                                             =>
-        },
-        // selection changes as a result of ongoing mouse actions
-        mouseAction.signal --> {
-          case a: ExtendSelectionAction   => viewerOps.onExtendSelectionAction(allSelectable)(a)
-          case a: AddNewArrowAction       => viewerOps.onAddNewArrowAction(a)
-          case a: MoveArrowEndpointAction => viewerOps.onMoveArrowSourceAction(a)
+    Seq(
+      // mouseAction.signal.map(_.name).distinct --> { c => pprint.log(c) },
+      transform --> { tr => firstGroup.setAttribute("transform", tr) },
+      // --------------------------------------------------------
+      // Mouse events
+      // --------------------------------------------------------
+      // 1. Drawing a selecting rectangle starts here. Other actions start in their respective elements.
+      onMouseDown.filter(leftButton).map(clientCoords) --> { (pos, shift) =>
+        mouseAction.start(ExtendSelectionAction(MouseActionRect(pos, pos, shift)))
+      },
+      // 2. Any ongoing action is updated here (i.e., mouse position)
+      onMouseMove.filter(leftButtonMoved).map(clientCoords) --> mouseAction.updateEndpoint.tupled,
+      // 3. Any ongoing action ends here
+      onMouseUp.filter(leftButton)(_.withCurrentValueOf(mouseAction.signal)) --> { (ev, previousAction) =>
+        mouseAction.inactive()
+        previousAction match
+          case a: AddNewArrowAction       => viewerOps.handleAddNewArrowMouseUp(ev, a)
+          case a: MoveArrowEndpointAction => viewerOps.handleMoveArrowStartMouseUp(ev, a)
+          case a: ExtendSelectionAction   =>
           case Inactive                   =>
-        },
-        // --------------------------------------------------------
-        //   synchronize svg elements with diagramSelection
-        // --------------------------------------------------------
-        selectionGroups --> { (toUnselect: Seq[SelectableElement], toSelect: Seq[SelectableElement]) =>
-          toUnselect.foreach(_.unselect())
-          toSelect.foreach(_.select())
-          // select/unselect modify the DOM directly, which seems to make the focus go to the
-          // document body. We need the focus back to the canvas container to process handle keys.
-          dom.window.requestAnimationFrame(_ => viewerOps.canvasContainerFocus.set(true))
-          ()
-        }
-      )
-    }
+      },
+      // --------------------------------------------------------
+      // derived events
+      // --------------------------------------------------------
+      // selection rectangle
+      child.maybe <--
+        mouseAction.signal.map:
+          case a: ExtendSelectionAction => Some(viewerOps.DrawSelectionRect(topLevelSvg.ref, a))
+          case _                        => None,
+      // controls to initiate mouse actions
+      singleSelection.combineWith(mouseAction.signal) --> viewerOps.handleNewArrowControls(firstGroup).tupled,
+      singleSelection.combineWith(mouseAction.signal) --> viewerOps.handleArrowEndpointControl(firstGroup).tupled,
+      // dynamic arrow that follows the pointer
+      mouseAction.signal --> { action =>
+        // TODO: update the coordinates instead of recreating the arrow
+        firstGroup.querySelectorAll("g#dragging-arrow-group").foreach(_.remove())
+        action match
+          case a: AddNewArrowAction if !a.rect.isEmpty       => viewerOps.addArrowFromSourceToPointer(firstGroup, a)
+          case a: MoveArrowEndpointAction if !a.rect.isEmpty => viewerOps.addArrowBetweenPointerAndEndpoint(firstGroup, a)
+          case _                                             =>
+      },
+      // selection changes as a result of ongoing mouse actions
+      mouseAction.signal --> {
+        case a: ExtendSelectionAction   => viewerOps.onExtendSelectionAction(allSelectable)(a)
+        case a: AddNewArrowAction       => viewerOps.onAddNewArrowAction(a)
+        case a: MoveArrowEndpointAction => viewerOps.onMoveArrowSourceAction(a)
+        case Inactive                   =>
+      },
+      // --------------------------------------------------------
+      //   synchronize svg elements with diagramSelection
+      // --------------------------------------------------------
+      selectionGroups --> { (toUnselect: Seq[SelectableElement], toSelect: Seq[SelectableElement]) =>
+        toUnselect.foreach(_.unselect())
+        toSelect.foreach(_.select())
+        // select/unselect modify the DOM directly, which seems to make the focus go to the
+        // document body. We need the focus back to the canvas container to process handle keys.
+        dom.window.requestAnimationFrame(_ => viewerOps.canvasContainerFocus.set(true))
+        ()
+      }
+    )
+  }
 end SvgCanvas
 
 /** Creates a standalone SVG element with the given viewBox
